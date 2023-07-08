eadb21c062301a2ebeea30f78ed39d8a3bb1be9b
Laika Patel <laikakpatel@gmail.com>
2023-07-07 21:53:17 -0500

    Initial Commit

---
+++ EsteeLauderProj/EsteeLauderProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  EsteeLauderProjApp.swift
+//  EsteeLauderProj
+//
+//  Created by Laika Patel on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct EsteeLauderProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
